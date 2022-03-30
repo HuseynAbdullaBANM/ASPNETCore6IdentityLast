415c52f6b397d3cb9dbc3ed2a37716296124ebdc
HuseynAbdullaBANM <huseyn.abdullayev.std@bhos.edu.az>
2022-03-30 19:42:35 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCore6IdentityLast.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
